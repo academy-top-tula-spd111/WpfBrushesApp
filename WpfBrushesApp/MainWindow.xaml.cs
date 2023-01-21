dd9365667c704ad2ea8f1b568208cca3f62cabea
maxefimov <efimov.academy.top@gmail.com>
2023-01-21 15:43:54 +0300

    Добавьте файлы проекта.

---
+++ WpfBrushesApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBrushesApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btn2.Background = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+            /*
+            var gradient = new LinearGradientBrush();
+            gradient.StartPoint = new Point(0, 0);
+            gradient.EndPoint = new Point(1, 0);
+            gradient.GradientStops.Add(new GradientStop() { Color = Colors.Aqua, Offset = 0 });
+            gradient.GradientStops.Add(new GradientStop() { Color = Colors.Khaki, Offset = 1});
+            btn4.Background = gradient;
+            */
+        }
+    }
+}
